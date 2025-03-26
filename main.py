07f1b58ca7d44f9538be379860e9568871f5cf1f
Vladyslav Husenko <qmd4325621@gmail.com>
2025-03-26 15:31:37 +0200

    Added an opportunity to read from file

---
+++ main.py
@@ -0,0 +1,18 @@
+# Зчитування даних з файлу
+def read_population_data(file_path):
+    countries = []
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            parts = line.strip().split(',')
+            if len(parts) == 3:
+                name, area, population = parts
+                try:
+                    area = float(area)
+                    population = int(population)
+                    countries.append((name, area, population))
+                except ValueError:
+                    print(f"Невірний формат даних у рядку: {line}")
+
+    return countries
+
