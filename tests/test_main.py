87b913a1cb3112b546865a460b31c03e026a4ab5
Vladyslav Husenko <qmd4325621@gmail.com>
2025-03-26 16:24:14 +0200

    Modified test

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,25 +5,27 @@ from main import read_population_data, sort_by_population, sort_by_area
 def mock_population_data():
     # Фікстура, яка повертає дані для тестування
     return [
-        ("Україна", 1, 4),
-        ("Німеччина", 2, 3),
-        ("Італія", 3, 2)
+        ("Україна", 603628, 41660900),
+        ("Німеччина", 357022, 83166711),
+        ("Італія", 301340, 60262770)
     ]
 
 @pytest.mark.parametrize("data, expected", [
-    ([( "Україна", 603628, 41660900), ("Німеччина", 357022, 83166711)], "Україна"),
-    ([( "Італія", 301340, 60262770), ("Німеччина", 357022, 83166711)], "Німеччина"),
+    ([("Україна", 603628, 41660900), ("Німеччина", 357022, 83166711)], "Україна"),
+    ([("Україна", 301340, 60262770), ("Німеччина", 357022, 83166711)], "Україна"),
 ])
 def test_sort_by_area(data, expected):
+    # Тестуємо сортування за площею
     sorted_data = sort_by_area(data)
     assert sorted_data[0][0] == expected
 
 def test_sort_by_population(mock_population_data):
+    # Тестуємо сортування за населенням
     sorted_data = sort_by_population(mock_population_data)
-    assert sorted_data[0][0] == "Ukraine"
-    assert sorted_data[1][0] == "Italy"
-    assert sorted_data[2][0] == "Germany"
+    assert sorted_data[0][0] == "Німеччина"
+    assert sorted_data[1][0] == "Україна"
+    assert sorted_data[2][0] == "Італія"
 
-def test_read_population_data():
-    data = read_population_data('mock_population_data.txt')
-    assert len(data) > 0  # Перевірка, що дані зчитано
+def test_read_population_data(mock_population_data):
+    data = read_population_data('population_data.txt')
+    assert len(mock_population_data) > 0  # Перевірка, що дані зчитано
